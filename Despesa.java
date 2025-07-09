43a25b04653f55068094cb7cf5220645d294af3d
Anthony Matheus <anthonymnf30@gmail.com>
2025-07-09 17:30:03 -0300

    :sparkles: feat: Adicionando as funcionalidades de excluir, visualizar, listar, buscar e editar despesa

--- Despesa.java
+++ Despesa.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Despesa {
   private String idDespesa;
@@ -6,11 +8,47 @@ public class Despesa {
   private Double valor;
   private Date data;
 
+  // Lista estática para armazenar todas as instâncias de despesas
+  private static List<Despesa> listaDespesas = new ArrayList<>();
+
   public Despesa(String idDespesa, String nomeDespesa, Double valor, Date data) {
     this.idDespesa = idDespesa;
     this.nomeDespesa = nomeDespesa;
     this.valor = valor;
     this.data = data;
+
+    listaDespesas.add(this);
+  }
+
+  public boolean excluirDespesa() {
+    return listaDespesas.remove(this);
+  }
+
+  // Visualizar a própria despesa
+  public Despesa visualizarDespesa() {
+    return this;
+  }
+
+  // Listar todas as despesas
+  public static List<Despesa> listarDespesa() {
+    return listaDespesas;
+  }
+
+  // Buscar despesa por ID (estático)
+  public static Despesa buscarDespesa(String id) {
+    for (Despesa d : listaDespesas) {
+      if (d.getIdDespesa().equalsIgnoreCase(id)) {
+        return d;
+      }
+    }
+    return null;
+  }
+
+  public boolean editarDespesa(String novoNome, double novoValor, Date novaData) {
+    this.nomeDespesa = novoNome;
+    this.valor = novoValor;
+    this.data = novaData;
+    return true;
   }
 
   public String getIdDespesa() {
